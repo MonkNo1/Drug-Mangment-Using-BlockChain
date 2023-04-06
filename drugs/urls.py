2254cde6495d556ee9e17a4e9154868319b3015d
Monk <61575072+MonkNo1@users.noreply.github.com>
2023-04-06 19:39:51 +0530

    features removed

--- drugs/urls.py
+++ drugs/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blog', views.blog, name='blog'),
-    path('blogsingle', views.blogsingle, name='blogsingle'),
-    path('features', views.features, name='features'),
+    # path('blog', views.blog, name='blog'),
+    # path('blogsingle', views.blogsingle, name='blogsingle'),
+    # path('features', views.features, name='features'),
     path('pricing', views.pricing, name='pricing'),
     path('contact', views.contact, name='contact'),
     path('login', views.login, name='login'),
